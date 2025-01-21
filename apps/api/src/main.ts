efd4dba1caab64a175b241790ccd0d27e74962d1
Raktim Shrestha <rakimsth@gmail.com>
2025-01-22 01:14:52 +0545

    added eslint + prettier + ts setup

---
+++ apps/api/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 4444);
+  console.log('App is running: http://localhost:4444');
+}
+bootstrap();
---
+++ apps/web/.prettierrc.mjs
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+import * as prettierBase from "@workspace/eslint-config/prettier-base";
+
+export default {
+  ...prettierBase,
+};
