efccc2f024e4b7ef0899183767bb33e9f7096082
Raktim Shrestha <rakimsth@gmail.com>
2025-03-06 12:38:55 +0545

    fixed suspense issue

--- apps/web/app/auth/login/page.tsx
+++ apps/web/app/auth/login/page.tsx
@@ -1,14 +1,5 @@
 'use client';
 
-// import dynamic from 'next/dynamic';
-
-// const AuthPage = dynamic(
-//   () => import('@/sections/auth/main').then((mod) => mod.AuthMain),
-//   { ssr: false },
-// );
-
-// export default AuthPage;
-
 import { Suspense } from 'react';
 import LoginPage from './login';
 
